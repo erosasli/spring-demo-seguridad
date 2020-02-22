f1c9ccb5bb8706974cf792ad142e1fd34e922982
erosas <erosas.lizama@gmail.com>
2020-02-22 19:22:36 -0300

    seguridad demo csrf

---
+++ spring-security-demo-csrf/src/main/java/com/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+/**
+ * Represta el filtro que usa spring security
+ * @author erosas
+ *
+ */
+public class SecurityWebApplicationInitializer  extends AbstractSecurityWebApplicationInitializer{
+
+}
